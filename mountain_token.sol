8a5452e770c474f96e9592c2ef96f84f5e3505ff
paaras2004 <90059820+paaras2004@users.noreply.github.com>
2024-06-22 01:11:45 +0530

    Create mountain_token.sol

---
+++ mountain_token.sol
@@ -0,0 +1,15 @@
+//eth-intermediate/mountain_token.sol;
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.26;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+contract mountain_tokken is ERC20{
+    constructor(uint256 initialSupply) ERC20("mountain_token","mou"){
+        _mint(msg.sender,initialSupply);
+
+    }
+    function burn(uint256 value) public{
+        _burn(msg.sender, value);
+    }
+    
+ 
